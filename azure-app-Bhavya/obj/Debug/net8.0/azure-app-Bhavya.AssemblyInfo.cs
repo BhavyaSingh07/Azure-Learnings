c4a87a2518d4739902aab646d940b1edfd308c38
BhavyaSingh07 <bhavyasingh1101@gmail.com>
2024-12-09 14:14:38 +0530

    Updated

--- azure-app-Bhavya/obj/Debug/net8.0/azure-app-Bhavya.AssemblyInfo.cs
+++ azure-app-Bhavya/obj/Debug/net8.0/azure-app-Bhavya.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azure-app-Bhavya")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d81e3807ebd13168d0e1ebb48770a02b6ba1a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("azure-app-Bhavya")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azure-app-Bhavya")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
